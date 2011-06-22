fb8a0502884a8f03579c42a78fc097acc6d6fb11
Ondřej Surý <ondrej@sury.org>
2011-06-22 09:10:39 +0200

    Imported Upstream version 2.4.9

--- xversion.h
+++ xversion.h
@@ -1,3 +1,3 @@
-/* Release cyrus-imapd-2.4.9beta2 */
-#define _CYRUS_VERSION "v2.4.9beta2"
-#define CYRUS_GITVERSION "6a5a394d 2011-06-14"
+/* Release cyrus-imapd-2.4.9 */
+#define _CYRUS_VERSION "v2.4.9"
+#define CYRUS_GITVERSION "ebb103d8 2011-06-21"
