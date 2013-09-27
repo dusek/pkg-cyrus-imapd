99ef10e8adc97acc0f2943b1a43a8b745a0f0adf
Ondřej Surý <ondrej@sury.org>
2013-09-27 08:54:41 +0200

    New upstream version 2.4.17+caldav~beta7

--- xversion.h
+++ xversion.h
@@ -1,3 +1,3 @@
-/* Release cyrus-imapd-2.4.17-caldav-beta6 */
-#define _CYRUS_VERSION "v2.4.17-caldav-beta6"
-#define CYRUS_GITVERSION "5e525dd0 2013-07-01"
+/* Release cyrus-imapd-2.4.17-caldav-beta7 */
+#define _CYRUS_VERSION "v2.4.17-caldav-beta7"
+#define CYRUS_GITVERSION "954b0f07 2013-09-18"
